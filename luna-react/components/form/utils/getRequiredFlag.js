bb14b4b23687f9574314a7bc42fd4f3db999c835
Ed Mortlock <edward.mortlock@sainsburys.co.uk>
2018-07-02 13:50:40 +0100

    CodeSandbox only handles 300 files

--- luna-react/components/form/utils/getRequiredFlag.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-
-exports.default = function (required, optional) {
-  return required !== undefined || optional !== undefined ? !!required : undefined;
-};
