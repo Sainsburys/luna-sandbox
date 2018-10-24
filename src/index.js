741ebf84284ab39e4bdf41316bc371444ae51453
WebSeed <Will.Bamford@sainsburys.co.uk>
2018-10-24 15:18:58 +0100

    Lose the ability to sass for now

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import ReactDOM from 'react-dom'
 import { Container, Card, PrimaryButton } from '../luna-react'
 import { Favourites } from '../luna-react/icons'
 
-// import './index.scss'
-
 function App() {
   return (
     <Container soft className="ln-u-soft-top" style={{ maxWidth: '600px', textAlign: 'center' }}>
-      <Card padded className="c-example-component">
+      <Card padded>
         <h1 className="ln-u-color-alpha">
           Welcome to Luna!
         </h1>
         <PrimaryButton onClick={() => alert('🎉')}><Favourites className="ln-u-margin-right"/> Tada!</PrimaryButton>
         <p className="ln-u-push-top">Fork this sandbox if you want to try out Luna for yourself.</p>
-        <p>Check out our <a target="_blank" href="https://jsainsburyplc.github.io/luna-react/">documentation</a> to browse through the components that are available and how to customise these.</p>
+        <p>Browse the <a target="_blank" href="https://jsainsburyplc.github.io/luna-react/">documentation</a> to see what components are available and information on how to configure these.</p>
       </Card>
     </Container>
   )
